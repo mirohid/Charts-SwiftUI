a71d06c24fc4fbe8d97ff2a1172350687303c36d
mirohid <mirohid7860@gmail.com>
2025-02-12 10:33:33 +0530

    customise pie chart

--- ChartsSwiftUI/PieChart.swift
+++ ChartsSwiftUI/PieChart.swift
@@ -20,10 +20,11 @@ struct PieChart: View {
     var body: some View {
         Chart{
             ForEach (data) { d in
-                SectorMark (angle: . value ( "Hours",
-                                             d . hours ) )
-                . foregroundStyle ( by :
-                        . value ( "Day", d.day) )
+                SectorMark(angle: .value("Hours",d .hours ),
+                           innerRadius: .ratio(0.5), angularInset: 2
+                        )
+                . foregroundStyle ( by : .value("Day", d.day) )
+                .cornerRadius(20)
             }
         }.padding()
     }
